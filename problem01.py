6bfefc25cdd7e5793393e7c70acbc5931aa48671
asd1025 <tmwrsts@daum.net>
2019-06-13 18:53:12 +0900

    .

--- problem01.py
+++ problem01.py
@@ -1,13 +1,11 @@
 # 문제1. 파이썬 경로명 s = '/usr/local/bin/python' 에서 각각의 디렉토리 경로명을 분리하여
 # 출력하세요.
 
-str = input('s = ')
-split_str='/usr/local/bin/'
-result2 = str.split(split_str)
-result1 = str.split('/')
+s = '/usr/local/bin/python'
+
+result2 = s.rsplit('/',1)
+result1 = s.split('/')
 del result1[0]
-del result2[0]
-result2.insert(0,split_str)
 
 print('실행 결과 :\n',result1)
 print ('실행 결과 :\n', result2)
